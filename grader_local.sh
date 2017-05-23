8a5ed4b4403ee989fb86b6392b8e322e44f9af9c
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-23 11:51:22 -0700

    blah

---
+++ grader_local.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+shopt -s extglob
+
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
+git diff --name-only ${branch}..key >> FILENAME_RESULTS
+
+git checkout key
+
+for FILE in $(cat FILENAME_RESULTS)
+do    
+  NAME=${FILE//+(*\/|\.*)}
+  LAST=($find -name NAME -print0 -quit)  
+  echo ${NAME}
+  git diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn: %L" ${branch}:FILE key:LAST
+  echo
+done
+
+git checkout ${branch}
+rm FILENAME_RESULTS
