3af9d3afb602dccbb1f34f230e2e03fb4b77c582
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-23 11:10:27 -0700

    yay

--- grader.sh
+++ grader.sh
@@ -6,14 +6,16 @@ branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 git diff --name-only ${branch}..key >> FILENAME_RESULTS
 
+git checkout key
+
 for FILE in FILENAME_RESULTS
 do    
   NAME=${FILE//+(*\/|\.*)}
-  git checkout key
   LAST=($find -name NAME -print0 -quit)  
   echo ${NAME}
-  git diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn: %L" branch:FILE key:LAST
+  git diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn: %L" ${branch}:FILE key:LAST
   echo
 done
 
+git checkout ${branch}
 rm FILENAME_RESULTS
