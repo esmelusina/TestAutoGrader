1fdf949bcddd4dcbb8b5cc4c58cc92517afa0751
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-24 12:47:16 -0700

    blah

--- grader.sh
+++ grader.sh
@@ -6,25 +6,16 @@ shopt -s extglob
 commit=$(git show --oneline -s --format="%h")
 
 # Fetch all the files that differ from the answer key branch and the commit
-git diff --name-only ${commit}..origin/key >> FILENAME_RESULTS
+git diff --name-only --diff-filter=M ${commit}..origin/key >> FILENAME_RESULTS
 
 # for each file that has differences
 for FILE in $(cat FILENAME_RESULTS)
 do  
   echo ${FILE}  
-  #NAME=${FILE//+(*\/|\.*)}
-  git rev-parse --verify --quiet origin/key:${FILE} >/dev/null
-  ec1=$?
-  git rev-parse --verify --quiet ${commit}:${FILE} >/dev/null
-  ec2=$?
-  echo $ec1
-  echo $ec2
-    
     diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " <(git show --quiet ${commit}:${FILE}) <(git show --quiet origin/key:${FILE})
-  #fi
    echo
 done
 
 #unset NAME
 #unset commit
-rm FILENAME_RESULTS
+#rm FILENAME_RESULTS
