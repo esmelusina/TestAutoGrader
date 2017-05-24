63d25d0a4df7defb60d624dd7e876e074f5cc97c
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-24 13:21:17 -0700

    yay

--- grader.sh
+++ grader.sh
@@ -5,17 +5,25 @@ shopt -s extglob
 #
 commit=$(git show --oneline -s --format="%h")
 
+branch=${1}
+
+if !$branch
+then
+branch=origin/key
+fi
+
 # Fetch all the files that differ from the answer key branch and the commit
-git diff --name-only --diff-filter=M ${commit}..origin/key >> FILENAME_RESULTS
+git diff --name-only --diff-filter=M ${commit} ${branch} >> FILENAME_RESULTS
+
 
 # for each file that has differences
 for FILE in $(cat FILENAME_RESULTS)
 do  
   echo ${FILE}  
-    diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " <(git show --quiet ${commit}:${FILE}) <(git show --quiet origin/key:${FILE})
+    diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " <(git show --quiet ${commit}:${FILE}) <(git show --quiet ${branch}:${FILE})
    echo
 done
 
-#unset NAME
-#unset commit
-#rm FILENAME_RESULTS
+unset commit
+unset branch
+rm FILENAME_RESULTS
