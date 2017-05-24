061790a09a95e0c195c3f485f37e1e62cb9bbd5a
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-24 10:32:04 -0700

    blah

--- grader.sh
+++ grader.sh
@@ -3,6 +3,8 @@
 shopt -s extglob
 
 #branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+#branch=$(git show --oneline -s --format="%h")
+
 branch=$(git show --oneline -s --format="%h")
 
 git diff --name-only ${branch}..origin/key >> FILENAME_RESULTS
@@ -11,9 +13,9 @@ git diff --name-only ${branch}..origin/key >> FILENAME_RESULTS
 for FILE in $(cat FILENAME_RESULTS)
 do    
   NAME=${FILE//+(*\/|\.*)}
-  LAST=$(git ls-tree -r --name-only master | head -2 | grep ${NAME})
+  #LAST=$(git ls-tree -r --name-only origin/key | grep "\b${NAME}\b")
   echo ${NAME}
-  diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn " <(git show ${branch}:FILE) <(git show key:LAST)
+  diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn " <(git show ${branch}:${FILE}) <(git show origin/key:${FILE})
   echo
 done
 
