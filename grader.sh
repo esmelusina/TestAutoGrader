ada158663dc136e76bdf1ab1626fd559291d6ee4
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-24 13:39:23 -0700

    blah

--- grader.sh
+++ grader.sh
@@ -19,8 +19,10 @@ git diff --name-only --diff-filter=M ${commit} ${branch} >> FILENAME_RESULTS
 # for each file that has differences
 for FILE in $(cat FILENAME_RESULTS)
 do  
+git show ${commit}:${FILE}
+git show ${branch}:${FILE}
   echo ${FILE}  
-    diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " <(git show --quiet ${commit}:${FILE}) <(git show --quiet ${branch}:${FILE})
+    diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " <(git show ${commit}:${FILE}) <(git show ${branch}:${FILE})
    echo
 done
 
