3431c09451b3ccd759c7d3f24b9a8973df622794
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-25 15:30:08 -0700

    rdy to test?

--- prog_grader.sh
+++ prog_grader.sh
@@ -1,12 +1,29 @@
+diff-line()
+{
+  src=$1
+  sub=$2  
+  lin=0
+
+  while read srcline <$src || [ -n "$srcline" ]
+  do
+    read subline <$sub
+    lin=$((lin+1))
+    if [ "$subline" != "$srcline" ] ;  then printf "$lin " ; fi    
+  done
+}
+
+
 commit=$(git show --oneline -s --format="%h")
 branch=${1}
+name=$TRAVIS_REPO_SLUG
 
-# let's find any source files that are added
+if [[ -z $branch ]]; then branch=origin/key; fi
+if [[ $name ]]; then name=$(dirname $name); else name="NULL"; fi
+
+echo $name
+
 
 for FILE in $(git diff --name-only --diff-filter=A ${branch} ${commit} | grep *.cpp)
-
-
-
 do
     BASE=${FILE%%.*}
     gcc $BASE.cpp -o $BASE.exe
