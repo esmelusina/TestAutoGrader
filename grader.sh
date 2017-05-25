248a0a65afc3c5fa7bd5792ca65dbd6c86d927e9
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-25 12:15:02 -0700

    blah

--- grader.sh
+++ grader.sh
@@ -2,7 +2,6 @@
 
 shopt -s extglob
 
-
 diff-line()
 {
   sub=$1
@@ -18,27 +17,19 @@ diff-line()
   echo
 }
 
-
-#
 commit=$(git show --oneline -s --format="%h")
 
 branch=${1}
 
 if [[ -z $branch ]]; then branch=origin/key; fi
 
-# Fetch all the files that differ from the answer key branch and the commit
-git diff --name-only --diff-filter=M ${commit} ${branch} >> FILENAME_RESULTS
 
-
-# for each file that has differences
-for FILE in $(cat FILENAME_RESULTS)
+for FILE in $(git diff --name-only --diff-filter=M ${commit} ${branch})
 do  
   echo ${FILE}  
     diff-line <(git show ${commit}:${FILE}) <(git show ${branch}:${FILE})
-    diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " <(git show ${commit}:${FILE}) <(git show ${branch}:${FILE})
    echo
 done
 
 unset commit
 unset branch
-rm FILENAME_RESULTS
