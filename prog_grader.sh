29587a4307283940ca9f5df7b4f857e82a909ad0
Esmeralda Salamone <esmes@sea.aie.edu>
2017-05-25 17:32:03 -0700

    wtf

--- prog_grader.sh
+++ prog_grader.sh
@@ -35,11 +35,12 @@ do
       g++ $BASE.cpp -o $BASE
       chmod +x $BASE
 
-      git show ${branch}:$BASE.in &>/dev/null
-      if [[ $? -ne 0 ]]
-      then diff-line <(git show ${branch}:$BASE.eo) <(./$BASE)
-      else diff-line <(git show ${branch}:$BASE.eo) <(./$BASE <(git show ${branch}:$BASE.in) )      
-      fi
+      #git show ${branch}:$BASE.in &>/dev/null
+      #if [[ $? -ne 0 ]]
+      #then diff-line <(git show ${branch}:$BASE.eo) <(./$BASE)
+      #else
+      diff-line <(git show ${branch}:$BASE.eo) <(./$BASE <(git show ${branch}:$BASE.in) )      
+      #fi
     fi      
 done
 
